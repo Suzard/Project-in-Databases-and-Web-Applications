177da2080dc8e80c9414a53c7c08529e6a65a403
ssgandham <sgandham@uci.edu>
2017-05-10 12:01:28 -0700

    the stars firstname, lastname adn dob is gettign added to the database

--- Project 3/src/data_files/parser.java
+++ Project 3/src/data_files/parser.java
@@ -1,33 +1,41 @@
 package data_files;
+import java.awt.List;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeSet;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
-
-import data_files.*;
+import java.sql.Date;
 public class parser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Reference :
 		// https://www.tutorialspoint.com/java_xml/java_dom_parse_document.htm
+		Map<String, Object> map_stars = new LinkedHashMap<String, Object>();
+		TreeSet<String> set = new TreeSet<String>();
 		
-			
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
 				
@@ -39,6 +47,21 @@ public class parser {
 				else {
 					System.out.println("Connection Successfull");
 				}
+				
+			String query_stars = "select * from stars";
+			Statement select_stars = (Statement) test_connection.createStatement();
+			ResultSet result_stars = select_stars.executeQuery(query_stars);
+			
+			while(result_stars.next()){
+				String first_name1="", last_name1="";
+				ArrayList<Object> list1 = new ArrayList<Object>();
+				if(result_stars.getString("first_name")!=null)
+				first_name1 = result_stars.getString("first_name").toString();
+				last_name1  = result_stars.getString("last_name").toString();
+				list1.add(first_name1 + "," + last_name1);
+				map_stars.put(first_name1 + last_name1, list1);
+			}
+			
 			File inputFile_stars = new File("src/data_files/actors63.xml");
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
@@ -47,19 +70,15 @@ public class parser {
 
 			
 			
-			System.out.println("Hi 1");
+
 			Document document = builder.parse(inputFile_stars);
-			System.out.println("Hi 2");
 			document.normalize();
 			NodeList list_actors = document.getElementsByTagName("actors");
-			System.out.println("Hi 3");
 			Node node_actors = list_actors.item(0);
-			System.out.println("Hi 4");
 			Element element_actors = (Element) node_actors;
-			System.out.println("Hi 5");
 			NodeList list_actor = element_actors.getElementsByTagName("actor");
 			System.out.println("Length :" + list_actor.getLength());
-			for (int i = 0; i < 1300; i++) {
+			for (int i = 0; i < list_actor.getLength(); i++) {
 				
 				String firstname="", lastname="",dob="";
 				Node node_actor = list_actor.item(i);
@@ -68,10 +87,10 @@ public class parser {
 				Node node_actor_firstname = element_actor.getElementsByTagName("firstname").item(0);
 				Node node_actor_lastname  = element_actor.getElementsByTagName("familyname").item(0);
 				Node node_actor_dob                  = element_actor.getElementsByTagName("dob").item(0);
-				Element element_actor_firstname = (Element) node_actor_firstname;
-				firstname=element_actor_firstname.getTextContent();
 				
 				try{
+				Element element_actor_firstname = (Element) node_actor_firstname;
+				firstname=element_actor_firstname.getTextContent().trim();
 				if(firstname==null) firstname="";
 				System.out.println("First Name : " +firstname);//+ element_child.getElementsByTagName("firstname").item(0).getTextContent());
 				}catch(Exception e){
@@ -81,7 +100,7 @@ public class parser {
 				
 				try{
 				Element element_actor_lastname = (Element) node_actor_lastname;
-				lastname=element_actor_lastname.getTextContent();
+				lastname=element_actor_lastname.getTextContent().trim();
 				if(lastname==null) lastname="";
 				System.out.println("Last Name : " + lastname);//element_child.getElementsByTagName("familyname").item(0).getTextContent());
 				}catch(Exception e){
@@ -91,19 +110,45 @@ public class parser {
 				
 				try{
 				Element element_actor_dob = (Element) node_actor_dob;
-				dob=element_actor_dob.getTextContent();
-				if(dob==null) dob="";
+				dob=element_actor_dob.getTextContent().trim();
+			
+				if(dob=="") dob="2001";
 					
+				if(!dob.matches("-?\\d+")) {dob="2001"; System.out.println("not matches");}
 					System.out.println("DOB : " + dob);//element_child.getElementsByTagName("dob").item(0).getTextContent());
 				}catch(Exception e){
 					e.printStackTrace();
-//					dob="";
-//					System.out.println("DOB : " + dob);
+
 					continue;
 					
 				}
+				PreparedStatement star_insert = null;
+				star_insert = test_connection.prepareStatement("insert into stars(first_name,last_name,dob) values(?,?,?);");
+				if(!map_stars.containsKey(firstname + lastname)){
+					star_insert.setString(1, firstname);
+					star_insert.setString(2, lastname);
+//					DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+//					Date myDate = formatter.parse(dob+"-01-01"));
+//					java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
+					star_insert.setDate(3, java.sql.Date.valueOf(dob+"-01-01"));
+					star_insert.execute();
+					map_stars.put(firstname+lastname, firstname +","+lastname+","+dob);
+				}
 			}
-
+			
+//			Iterator<String> it= set.iterator();
+//			
+//			while(it.hasNext()){
+//				int i=1;
+//				String  combined_stars = it.next();
+//				 for (String star_details: combined_stars.split(",")) {
+//					 
+//					 System.out.println(i + "." + "Star Details : " +star_details);
+//					 ++i;	
+//			      }
+//				 
+//				}
+			
 		} catch (SAXException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
