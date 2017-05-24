6844289ce4b419ed487079bbea7052403df58761
ssgandham <sgandham@uci.edu>
2017-05-23 19:32:36 -0700

    Android Code modified

--- Android_Assignment4/MyApplication/app/src/main/java/com/example/balaji/myapplication/MainActivity.java
+++ Android_Assignment4/MyApplication/app/src/main/java/com/example/balaji/myapplication/MainActivity.java
@@ -5,20 +5,21 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private EditText edit_text;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-
+        edit_text = (EditText) findViewById(R.id.edit_text);
     }
 
 }
