219093d6887ca6c99c3eb9317a2ffcfd0c8d58c5
ssgandham <sgandham@uci.edu>
2017-05-24 17:05:54 -0700

    New modified Code for login and password

--- Android_Assignment4_updated/Android_Assignment_4/app/src/main/java/com/example/balaji/android_assignment_4/MainActivity.java
+++ Android_Assignment4_updated/Android_Assignment_4/app/src/main/java/com/example/balaji/android_assignment_4/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.balaji.android_assignment_4;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +10,73 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit_username, edit_password;
+    Button button_submit;
+    SharedPreferences shared_preference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        edit_username = (EditText) findViewById(R.id.username);
+        edit_password = (EditText) findViewById(R.id.password);
+        button_submit = (Button) findViewById(R.id.submit_button);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        shared_preference = getSharedPreferences("login_preference", Context.MODE_PRIVATE);
+
+        button_submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                SharedPreferences.Editor editor = shared_preference.edit();
+                editor.putString("username", edit_username.getText().toString());
+                editor.putString("password", edit_password.getText().toString());
+                editor.apply();
+                editor.commit();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onResume() {
+        super.onResume();
+        get_shared_preference();
+//        shared_preference = getSharedPreferences("login_preference", Context.MODE_PRIVATE);
+//        edit_username.setText(shared_preference.getString("username", ""));
+//        edit_password.setText(shared_preference.getString("password", ""));
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onPause() {
+        super.onPause();
+        get_shared_preference();
+//        shared_preference = getSharedPreferences("login_preference", Context.MODE_PRIVATE);
+//        edit_username.setText(shared_preference.getString("username", ""));
+//        edit_password.setText(shared_preference.getString("password", ""));
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        get_shared_preference();
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onStop(){
+        super.onStop();
+        get_shared_preference();
+    }
+    
+    public void get_shared_preference(){
+        shared_preference = getSharedPreferences("login_preference",Context.MODE_PRIVATE);
+        edit_username.setText(shared_preference.getString("username",""));
+        edit_password.setText(shared_preference.getString("password",""));
+
     }
 }
+
